84d8c399b6993f882f1c4726655abab9454cff94
Shivansh Bhardwaj <40199178+Hunterq417@users.noreply.github.com>
2025-02-27 22:40:06 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -23,21 +23,20 @@ const Login = () => {
     <GoogleOAuthProvider clientId={clientId}>
       <div className="login-container">
         <div className="login-box">
-          {/* ApnaCart Logo with Tree Icon */}
+          
           <h1 className="login-logo">
             <FaTree className="tree-icon" /> ApnaCart
           </h1>
           <p className="welcome-text-login">Welcome to ApnaCart</p>
           <p className="tagline">Your One-Stop Solution for Seamless Shopping!</p>
 
-          {/* Login Form */}
           <form className="login-form">
             <input type="email" placeholder="Email Address" name="email" required />
             <input type="password" placeholder="Password" name="password" required />
             <button type="submit" className="login-btn">Login</button>
           </form>
 
-          {/* Sign-in Options */}
+        
           <p className="or-text">or sign in with</p>
           <div className="social-login">
             <GoogleLogin
